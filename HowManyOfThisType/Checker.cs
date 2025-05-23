a15612172b9d7b10fb217c4bf076f27c8df5d48f
André <imandreleandro@gmail.com>
2025-05-23 08:38:26 +0100

    files

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace HowManyOfThisType
+{
+    public class Checker
+    {
+
+        public static int HowManyOfType<T>(IEnumerable items)
+        {
+            int count = 0;
+            foreach (var item in items)
+            {
+                if (item is T) count++;
+            }
+
+            return count;
+        }
+        
+    }
+}
